c9144379b6e03dcc7ed32848af9a4c9dd04481f8
lorehuincahue <lorehuincahue@gmail.com>
2022-11-27 21:07:27 +0000

    ok

---
+++ src/js/component/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer () {
+return (
+<footer class="text-muted py-2 p-3 mb-2 bg-dark text-white">
+  <div class="container">
+    <p class="mb-1 text-white text-center">Copyright © 4Geeks 2022</p>
+
+  </div>
+</footer>
+
+)
+}
+
+export default Footer;
---
+++ src/js/component/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header () {
+return(
+    <section class="w-auto  p-3 mb-2 bg-light">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">A Warm Welcome!</h1>
+        <p class="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ullam dicta quae voluptatem illum blanditiis ipsum neque inventore veniam dolorum delectus animi. Qui maiores ex, voluptatum itaque quibusdam voluptatibus cupiditate.</p>
+        <p>
+          <a href="#" class="btn btn-primary my-2">Call to action!</a>
+          
+        </p>
+      </div>
+    </div>
+  </section>
+)
+}
+
+export default Header;
